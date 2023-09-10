0ce7693f35ac1ee12f8e379936209b8dc1d03efb
hossein <hosseinama1383@gmail.com>
2023-09-10 16:39:07 +0330

    hotels app created

--- guests/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-
-
-class Guest(models.Model):
-    guest = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,null=True)
-    
-    def __str__(self):
-        return str(self.guest)
