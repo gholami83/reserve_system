144ee5318c6e31ffe2659567ace7ba9138bb8677
hossein <hosseinama1383@gmail.com>
2023-09-10 15:57:08 +0330

    owner updated

--- reserve/models.py
+++ reserve/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from rooms.models import Room
 from owner.models import Owner
+from guests.models import Guest
 
 
 class Reserve(models.Model):
-    owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+    owner = models.ForeignKey(Guest,on_delete=models.CASCADE)
     room = models.ForeignKey(Room,on_delete=models.CASCADE)
     reserved = models.BooleanField(default=False)
